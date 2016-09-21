758bfd42f102ff24d368c9a25486c90a3efcef9d
Liam Considine <liam.considine@adtegrity.com>
2016-09-21 16:46:10 -0400

    Wrote a test for a TestSuite change the interface of run so that it takes a result

--- TestCase.py
+++ TestCase.py
@@ -1,12 +1,28 @@
 class TestResult:
     def __init__(self):
         self.runCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount = self.runCount + 1
 
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
     def summary(self):
-        return "%d run, 0 failed" % self.runCount
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
 
 class TestCase:
     def __init__(self, name):
@@ -18,11 +34,12 @@ class TestCase:
     def tearDown(self):
         pass
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
         self.tearDown()
-        return result
--- xUnit.py
+++ xUnit.py
@@ -4,25 +4,49 @@ from TestCase import *
 class TestCaseTest(TestCase):
 
     def testTemplateMethod(self):
-        self.test = WasRun("testMethod")
-        self.test.run()
-        assert("setUp testMethod tearDown " == self.test.log)
+        test = WasRun("testMethod")
+        result = TestResult()
+        test.run(result)
+        assert("setUp testMethod tearDown " == test.log)
 
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
+        result = TestResult()
+        test.run(result)
         assert("1 run, 0 failed" == result.summary())
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
+        result = TestResult()
+        test.run(result)
         assert("1 run, 1 failed" == result.summary())
 
+    def testFailedResultFormatting(self):
+        result = TestResult()
+        result.testStarted()
+        result.testFailed()
+        assert("1 run, 1 failed" == result.summary())
 
-TCT = TestCaseTest("testTemplateMethod")
-TCT.run()
-TCT.testResult()
-TCT.testFailedResult()
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        result = TestResult()
+        suite.run(result)
+        assert("2 run, 1 failed" == result.summary)
+
+suite = TestSuite()
+for test in ["testTemplateMethod", "testResult", "testFailedResultFormatting", "testFailedResultFormatting", "testFailedResult", "testSuite"]:
+    suite.add(TestCaseTest(test))
+result = TestResult()
+suite.run(result)
+print result.summary()
+
+
+# TCT = TestCaseTest("testTemplateMethod")
+# TCT.run()
+# TCT.testResult()
+# TCT.testFailedResult()
 
 # print TCT.test.__class__
 # print TCT.test.name
