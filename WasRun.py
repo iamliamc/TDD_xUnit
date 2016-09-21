20a63b0c31ddc912aa37870250ac66b69c548508
Liam Considine <liam.considine@adtegrity.com>
2016-09-21 14:16:42 -0400

    Start of chapter 20

--- WasRun.py
+++ WasRun.py
@@ -1,12 +1,11 @@
 from TestCase import TestCase
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self, name)
-        
+
     def testMethod(self):
         self.wasRun = 1
 
-    def run(self):
-        method = getattr(self, self.name)
-        method()
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
--- xUnit.py
+++ xUnit.py
@@ -2,10 +2,16 @@ from WasRun import WasRun
 from TestCase import TestCase
 
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetup)
 
 TestCaseTest("testRunning").run()
+print "Compiled and ran"
